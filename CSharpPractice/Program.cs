87bc41173d8cbe0eb8dce6173add28ffc4e99c65
TrollHax <91326933+TrollHax@users.noreply.github.com>
2022-06-17 14:00:12 +0200

    Add project files.

---
+++ CSharpPractice/Program.cs
@@ -0,0 +1,29 @@
+﻿
+string name = PlanetRadius.Earth.ToString();
+int radius = (int)PlanetRadius.Earth;
+double volume = Volume(PlanetRadius.Earth);
+
+Console.WriteLine("Planet: " + name);
+Console.WriteLine("Radius: " + radius + "km");
+Console.WriteLine("Volume: " + volume + "km^3");
+
+static double Volume(PlanetRadius radius)
+{
+    double volume = Math.Round(((4 * Math.PI * Math.Pow((int)radius, 3)) / 3));
+    return volume;
+}
+
+Console.ReadKey();
+
+enum PlanetRadius
+{
+    Mercury = 2439,
+    Venus = 6051,
+    Earth = 6371,
+    Mars = 3389,
+    Jupiter = 69911,
+    Saturn = 58232,
+    Uranus = 25362,
+    Neptune = 24622,
+    Pluto = 1188
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] intArray = { 1, 2, 3 };
+double[] doubleArray = { 1.0, 2.0, 3.0 };
+string[] stringArray = { "1", "2", "3" };
+
+displayElements(intArray);
+displayElements(doubleArray);
+displayElements(stringArray);
+
+Console.ReadKey();
+
+static void displayElements<Thing>(Thing[] array)
+{
+    foreach (Thing item in array)
+    {
+        Console.Write(item);
+    }
+    Console.WriteLine();
+}
